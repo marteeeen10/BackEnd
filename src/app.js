89e87a36f5519a20ed09d8e11a94996c81be0dbb
Martin <marteeeen@gmail.com>
2023-05-30 01:12:56 -0300

    segunda entrega final

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 import express from "express";
-import ProductsRouter from "./routes/products.router.js";
-import CartsRouter from "./routes/carts.router.js";
+import ProductsRouter from "./routes/products.mongo.router.js";
+import CartsRouter from "./routes/carts.mongo.router.js";
 import viewsRouter from "./routes/views.router.js";
 import __dirname from './utils.js';
 import handlebars from 'express-handlebars';
 import { Server } from 'socket.io';
-import ProductManager from "../src/dao/fileSystem/Manager/productManager.js";
+import ProductsManager from "../src/dao/mongo/managers/productManager.js";
+import mongoose from "mongoose";
+import productModel from "../src/dao/mongo/models/products.js";
+import messagesModel from "../src/dao/mongo/models/messages.js";
 
 
 const app = express();
+const connection = mongoose.connect(
+  "mongodb+srv://martinpe:123@clustercomercio.eeuskzl.mongodb.net/ecommerce?retryWrites=true&w=majority"
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`));
@@ -21,11 +33,46 @@ app.use("/api/products", ProductsRouter);
 app.use("/api/carts", CartsRouter);
 app.use("/", viewsRouter);
 
-
 const server = app.listen(8080, () => console.log("escuchando en puertooo 8080"));
 const io = new Server(server);
 
-const renderRealTimeProducts = async (socket) => {
+io.on("connection", async (socket) => {
+  console.log("nuevo cliente conectado");
+  const productManager = new ProductsManager();
+  const products = await productManager.getProducts();
+  socket.emit("updateProducts", products);
+});
+
+const messages = [];
+
+io.on("connection", async (socket) => {
+  console.log("Nuevo clienteeee conectado");
+  try {
+    // Obtén todos los mensajes existentes desde MongoDB
+    const messages = await messagesModel.find({}).lean().exec();
+    socket.emit("logs", messages);
+  } catch (error) {
+    console.error("Error al obtener mensajes desde MongoDB:", error);
+  }
+  socket.on("message", async (data) => {
+    try {
+      // Crea un nuevo documento en MongoDB con el mensaje recibido
+      const message = await messagesModel.create(data);
+      // Agrega el nuevo mensaje al array en memoria
+      messages.push(message);
+      // Emite los mensajes actualizados a todos los clientes conectados
+      io.emit("logs", messages);
+    } catch (error) {
+      console.error("Error al crear el mensaje en MongoDB:", error);
+    }
+  });
+  socket.on("authenticated", (data) => {
+    socket.broadcast.emit("newUserConnected", data);
+  });
+});
+
+//viejo
+/* const renderRealTimeProducts = async (socket) => {
   try {
     const productManager = new ProductManager();
     const products = await productManager.getProducts();
@@ -37,4 +84,4 @@ const renderRealTimeProducts = async (socket) => {
 io.on("connection", (socket) => {
   console.log("nuevo cliente conectadoo");
   renderRealTimeProducts(socket);
-});
+}); */
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,19 +1,58 @@
 import { Router } from "express";
-import ProductManager from "../dao/fileSystem/Manager/productManager.js"
+import ProductsManager from "../dao/mongo/managers/productManager.js";
+import CartsManager from "../dao/mongo/managers/cartManager.js";
+import productModel from "../dao/mongo/models/products.js";
 
 const router = Router();
 
+const productsManager = new ProductsManager();
+const cartsManager = new CartsManager();
+
+router.get("/", async (req, res) => {
+  const { page = 1, sort = 1, limit = 10 } = req.query;
+
+  const options = {
+    page,
+    limit: parseInt(limit),
+    lean: true,
+    sort: { price: sort },
+  };
+
+  const { docs, hasPrevPage, hasNextPage, prevPage, nextPage, ...rest } =
+    await productModel.paginate({}, options);
+
+  // const result = await productModel.paginate({}, { limit: 3, lean: true });
+  // console.log(result);
+  const products = docs;
+
+  res.render("home", {
+    products,
+    page: rest.page,
+    hasPrevPage,
+    hasNextPage,
+    prevPage,
+    nextPage,
+    css: "products",
+  });
+});
+router.get("/realTimeProducts", async (req, res) => {
+  res.render("realTimeProducts", { css: "realTimeProducts" });
+});
+
+router.get("/cart", async (req, res) => {
+  const carts = await cartsManager.getCarts();
+  res.render("cart", { carts, css: "cart" });
+});
+
+router.get("/cart/:cid", async (req, res) => {
+  const cid = req.params.cid;
+  const carts = await cartsManager.getCarts();
+  const cartSelected = carts.find((cart) => cart._id == cid);
+  res.render("oneCart", { cartSelected, css: "cart" });
+});
+
+router.get("/chat", async (req, res) => {
+  res.render("chat", { css: "chat" });
+});
+
 export default router;
-
-const manager = new ProductManager();
-const products = manager.getProducts();
-
-router.get("/", async (rec, res) => {
-    const allProducts = await products;
-    res.render("home", { allProducts, css: "products" });
-  });
-  
-  router.get("/realTimeProducts", async (rec, res) => {
-    res.render("realTimeProducts", { css: "realTimeProducts" });
-  });
-
